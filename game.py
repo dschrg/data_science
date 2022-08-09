01d4774f8f554dd81b798a7c463e265b43b1811f
dschrg <dmg_01@yahoo.com>
2022-08-09 20:13:42 +0300

    the second commit

---
+++ game.py
@@ -0,0 +1,16 @@
+'''this programm divines a number'''
+import numpy as np
+number = np.random.randint(1,101) # we've picked a number
+count = 0
+while True:
+    count += 1
+    predict_number = int(input('Guess a number from 1 till 100: '))
+    
+    if predict_number > number:
+        print('Your number is more than predict_number!')
+    elif predict_number < number:
+        print('Your number is less than predict_number!')
+    else:
+        print(f'You have guessed the number! This number is {number}, for {count} tries')
+        break
+
---
+++ game_v2.py
@@ -0,0 +1,44 @@
+'''Game is called Guess the number. Computer predict and guesses numbers'''
+import numpy as np
+def score_game(random_predict) -> int:
+    """за какое количество попыток в среднем из 1000 подходов наш алгоритм угадает загаданное число
+
+    Args:
+        random_predict (_type_): guessing function
+
+    Returns:
+        int: average quntity of tries
+    """
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1,101, size = (1000))
+    for number in random_array:
+        count_ls.append(random_predict(number))
+    score = int(np.mean(count_ls))
+    
+    print(f'Ваш алгоритм угадывает число в среднем за {score} попыток')
+    return(score)
+
+    
+def random_predict(number: int=1) -> int:
+    """Guess a random number
+
+    Args:
+        number (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        int: quantity of tries
+    """
+    count = 0
+    while True:
+        count += 1
+        predict_number = np.random.randint(1,101) # a predict number
+        if number == predict_number:
+            break # exit from cycle
+    return(count)
+
+print(f'Quantity of tries: {random_predict()}')
+
+if __name__ == '__main__':
+    jscore_game(random_predict)
+
