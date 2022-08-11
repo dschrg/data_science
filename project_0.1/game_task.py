ec37ee849abf583289c38e032e9a08baa092d712
dschrg <dmg_01@yahoo.com>
2022-08-11 22:44:29 +0300

    commit project_0

--- project_0.1/game_task.py
+++ project_0.1/game_task.py
@@ -57,7 +57,6 @@ def score_game(random_predict) -> int:
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
 
-
 if __name__ == "__main__":
     # RUN
     score_game(random_predict)
